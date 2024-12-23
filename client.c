db673ea04b19d7524ca293982f575d66c2fb410d
tavolop3 <tavo.lop33@gmail.com>
2024-12-23 19:02:45 -0300

    con epoll no funca

--- client.c
+++ client.c
@@ -1,13 +1,17 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-// socket libraries
-#include <errno.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#define MAX_EVENTS 10
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
@@ -49,28 +53,69 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   freeaddrinfo(peer_address);
+  int status = // le agrega a los flags actuales el nonblock
+      fcntl(socket_peer, F_SETFL, fcntl(socket_peer, F_GETFL, 0) | O_NONBLOCK);
+  if (status == -1) {
+    perror("fcntl: non-block");
+  }
 
   printf("Conectado.\n");
   printf("Para enviar data, ingresá texto seguido de enter.\n");
-  char buffer[256];
 
-  bzero(buffer, 256);
-  fgets(buffer, 256, stdin);
-
-  int n = write(socket_peer, buffer, strlen(buffer));
-  if (n < 0) {
-    fprintf(stderr, "write() failed. (%d)\n", errno);
+  printf("Creando instancia de epoll...\n");
+  int epoll_fd = epoll_create1(0);
+  if (epoll_fd == -1) {
+    perror("epoll_create1");
     return 1;
   }
 
-  n = read(socket_peer, buffer, 255);
-  if (n < 0) {
-    fprintf(stderr, "read() failed. (%d)\n", errno);
-    return 1;
+  printf("Agregando el socket y stdin al epoll...\n");
+  struct epoll_event ev;
+  ev.events = EPOLLIN; // lectura
+  ev.data.fd = socket_peer;
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_peer, &ev) == -1) {
+    perror("epoll_ctl: socket_peer");
+    exit(EXIT_FAILURE);
+  }
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &ev) == -1) {
+    perror("epoll_ctl: stdin");
+    exit(EXIT_FAILURE);
   }
 
-  printf("%s\n", buffer);
+  int nfds;
+  struct epoll_event events[MAX_EVENTS];
+  for (;;) {
+    nfds =
+        epoll_wait(epoll_fd, events, MAX_EVENTS, -1); // timeout indefinido = -1
+    if (nfds == -1) {
+      perror("epoll_wait");
+      exit(EXIT_FAILURE);
+    }
 
+    for (int n = 0; n < nfds; ++n) {
+      if (events[n].data.fd == socket_peer) {
+        printf("Entre al sock peer\n");
+        char read[4096];
+        int bytes_received = recv(socket_peer, read, 4096, 0);
+        if (bytes_received == -1) {
+          perror("recv");
+          exit(EXIT_FAILURE);
+        }
+        printf("Recibidos (%d bytes): %.*s", bytes_received, bytes_received,
+               read); // %.*s prints a string of specified length
+      } else if (events[n].data.fd == 0) { // fd 0 = STDIN
+        printf("Entre al terminal\n");
+        char read[4096];
+        if (!fgets(read, 4096, stdin))
+          break;
+        printf("Enviando: %s", read);
+        int bytes_sent = send(socket_peer, read, strlen(read), 0);
+        printf("Enviados %d bytes.\n", bytes_sent);
+      }
+    }
+  }
+
+  printf("Cerrando el socket...\n");
   close(socket_peer);
 
   return 0;
